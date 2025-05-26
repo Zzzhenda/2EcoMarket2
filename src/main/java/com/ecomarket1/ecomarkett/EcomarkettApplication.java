1429abe957d2d9e259b28739e7f125f841ecf3e5
Zzzhenda <151049778+Zzzhenda@users.noreply.github.com>
2025-05-26 04:51:48 -0400

    Update EcomarkettApplication.java

--- src/main/java/com/ecomarket1/ecomarkett/EcomarkettApplication.java
+++ src/main/java/com/ecomarket1/ecomarkett/EcomarkettApplication.java
@@ -2,7 +2,7 @@ package com.ecomarket1.ecomarkett;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//m
 @SpringBootApplication
 public class EcomarkettApplication {
 
