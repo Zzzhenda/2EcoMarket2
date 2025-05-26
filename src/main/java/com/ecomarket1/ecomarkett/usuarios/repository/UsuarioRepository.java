9b36073dcb6959b056d646dae0a2582f0ab73b78
Zzzhenda <151049778+Zzzhenda@users.noreply.github.com>
2025-05-26 04:22:32 -0400

    autenticacion.java

--- src/main/java/com/ecomarket1/ecomarkett/usuarios/repository/UsuarioRepository.java
+++ src/main/java/com/ecomarket1/ecomarkett/usuarios/repository/UsuarioRepository.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Aquí puedes agregar métodos personalizados si es necesario
-    // Por ejemplo, para buscar usuarios por email o rut
+    //metodo para la autenticacion
     Optional<Usuario> findByEmail(String email);
+    
 }
  
